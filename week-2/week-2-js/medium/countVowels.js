4cd4a1669cae35fff115a8f8b4143e691b802060
katara-Jayprakash <katarajayprakash@icloud.com>
2025-06-18 23:38:46 +0530

    countVariable

--- week-2/week-2-js/medium/countVowels.js
+++ week-2/week-2-js/medium/countVowels.js
@@ -6,7 +6,14 @@
 */
 
 function countVowels(str) {
-    // Your code here
+   let vowelCnt = 0; 
+     str =  str.toLowerCase();
+   for(let i =0; i<str.length; i++){
+    if(str[i]=='a' || str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+      vowelCnt++; 
+    }
+   }
+   return vowelCnt; 
 }
 
 module.exports = countVowels;
