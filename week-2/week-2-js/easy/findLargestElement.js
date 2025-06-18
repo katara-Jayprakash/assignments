ba0090b46823b88c58913b14196da92b6f372a9d
katara-Jayprakash <katarajayprakash@icloud.com>
2025-06-18 23:26:02 +0530

    findLargestElement.js

--- week-2/week-2-js/easy/findLargestElement.js
+++ week-2/week-2-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+  let lrgstNum = numbers[0]; 
+     for(let i =0; i<numbers.length;i++){
+        if(numbers[i]>=lrgstNum){
+             lrgstNum = numbers[i];
+        }
+     }
+     return lrgstNum; 
 }
 
 module.exports = findLargestElement;
